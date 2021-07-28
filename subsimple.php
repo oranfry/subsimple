e7054e19c5989643bd08ff5534acc5fed7daaf5a
Oran Fry <oran@emerg.co>
2021-07-28 18:48:05 +1200

    Config
    Less reliance on config scripts to define constants.

--- subsimple.php
+++ subsimple.php
@@ -5,19 +5,28 @@ if (!defined('APP_HOME')) {
 }
 
 require __DIR__ . '/functions.php';
+require __DIR__ . '/src/php/class/Config.php';
+
+Config::set(require APP_HOME . '/config.php');
+
+define('PLUGINS', array_unique(array_merge(@$plugins ?: [], @Config::get()->requires ?? [])));
+
+if (Config::get()->root_username) {
+    define('ROOT_USERNAME', Config::get()->root_username);
+    define('ROOT_PASSWORD', @Config::get()->root_password);
+}
+
+if (!@Config::get()->db_home) {
+    die('db_home not set');
+}
+
+define('DB_HOME', Config::get()->db_home);
 
 define_autoloader();
-
-(function(){
-    $config = require APP_HOME . '/config.php';
-
-    Config::set($config);
-})();
-
-
 load_plugin_libs();
 init_plugins();
 route();
+
 $viewdata = do_controller();
 
 if (!defined('LAYOUT')) {
