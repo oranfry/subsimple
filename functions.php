4fd97dc089f1c2e8fb0befb5a26b6e0550559936
Oran Fry <oran@emerg.co>
2021-09-15 23:32:44 +1200

    Functions
    Implement dd to mimic laravel.

--- functions.php
+++ functions.php
@@ -1,5 +1,12 @@
 <?php
 
+function dd()
+{
+    call_user_func_array('var_dump', func_get_args());
+
+    die();
+}
+
 function define_autoloader()
 {
     spl_autoload_register(function ($class_name) {
