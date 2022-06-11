b64a08d101c53e59404a2bf35efc0ae88230b839
Oran Fry <oran@emerg.co>
2022-06-11 15:02:22 +1200

    Periods
    Add new period for sunday fortnight

---
+++ src/php/class/period/sundayfortnight.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace period;
+
+class sundayfortnight extends \Period
+{
+    public $id = 'sf';
+    public $navlabel = 'Sunday Fortnight';
+    public $step = '2 week';
+
+    public function label($from, $to)
+    {
+        $m1 = date('M', strtotime($from));
+        $m2 = date('M', strtotime($to));
+
+        $y1 = date('Y', strtotime($from));
+        $y2 = date('Y', strtotime($to));
+
+        return date('j' . ($m1 != $m2 ? ' M' : '') . ($y1 != $y2 ? ' Y' : ''), strtotime($from)) . " - " . date('j M Y', strtotime($to));
+    }
+
+    public function rawstart($date)
+    {
+        return date('Y-m-d', strtotime(date_shift($date, '-13 days')));
+    }
+
+    public function start($rawstart)
+    {
+        $time = strtotime(ff($rawstart, 'Sun'));
+        $fortnight_in_s = 1209600;
+        $week_in_s = 604800;
+        $x = $time / $fortnight_in_s;
+
+        if ($x - floor($x) >= 0.5) {
+            $time += $week_in_s;
+        }
+
+        return date('Y-m-d', $time);
+    }
+}
