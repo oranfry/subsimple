38f1681d66db8cef3562fa6b5733d137028ce42f
Oran Fry <oran@emerg.co>
2024-10-24 11:30:12 +1300

    Debug
    Change the appearance of dd etc.

--- src/php/dump/json/var_dump.php
+++ src/php/dump/json/var_dump.php
@@ -1,13 +1,13 @@
 <?php
 
-echo "\n[\n\n";
+echo "\n<<<<\n\n";
 
 foreach ($arguments as $i => $argument) {
     if ($i) {
-        echo "\n" . str_repeat('-', 20) . "\n\n";
+        echo "\n----\n\n";
     }
 
     var_dump($argument);
 }
 
-echo "\n]\n\n";
+echo "\n>>>>\n\n";
